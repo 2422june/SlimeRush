fc7a2c706d77a5b324f5d41cf97ce039e7c56eae
2422june <ck66661254@gmail.com>
2022-11-21 17:33:45 +0900

    add description

--- SlimeRush/Assets/Scripts/Camera/CameraController.cs
+++ SlimeRush/Assets/Scripts/Camera/CameraController.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 _different;
-    private Vector3 _moveDir;
-    private Vector3 _destination;
+    private Vector3 _difference;    ///Difference between player position and camera position
+    private Vector3 _moveDir;       ///Player movement direction
+    private Vector3 _destination;   ///Camera's destination
+
     [SerializeField]
-    private Transform _player;
+    private Transform _player;      ///The player transform
 
     void Update()
     {
+        /// If the player is not move, the camera is can't move.
+        if (_moveDir == Vector3.zero)
+            transform.position = Vector3.Lerp(transform.position, _player.position + _difference, Time.deltaTime * 6f);
+
+        /// move to the destination smoothly
         transform.position = Vector3.Lerp(transform.position, _destination, Time.deltaTime * 3f);
     }
 
     public void SetDestination(Vector3 moveDir)
     {
+        /// get player movement direction
         _moveDir = moveDir;
-        _destination = _moveDir + _player.position + _different;
+
+        _destination = _player.position + _moveDir + _difference;
     }
 }
