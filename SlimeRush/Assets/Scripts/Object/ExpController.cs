261d02c863e26d0e7aeeda117a302442f0484e75
2422june <ck66661254@gmail.com>
2022-12-20 00:10:35 +0900

    re made EXP Controller

---
+++ SlimeRush/Assets/Scripts/Object/ExpController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpController : MonoBehaviour
+{
+    [SerializeField]
+    private byte _type;
+
+    public int Contact()
+    {
+        gameObject.SetActive(false);
+        return Define.resultExps[_type];
+    }
+}
